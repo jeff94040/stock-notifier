20e688f0d221e45fe50bd94a14f089d3d5654482
Jeff Schmidt <jeff94040@gmail.com>
2022-01-19 20:26:43 -0800

    general updates

--- stock-notifier.js
+++ stock-notifier.js
@@ -1,56 +1,53 @@
 import fetch from 'node-fetch';
-import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
-//import config props from .env file
 dotenv.config();
 
-let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: "465",
-  secure: "true", //use TLS
-  auth: {
-    user: process.env.FROM_EMAIL_ADDRESS,
-    pass: process.env.FROM_EMAIL_PASSWORD
-  },
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: "false"
-  }
-})
+const millisecond_timer = 1000*60*5;
 
-// verify connection configuration
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+const items = [
+  {name: 'NVIDIA GeForce RTX 3080', stock: true, url: 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'},
+  {name: 'NVIDIA GeForce RTX 3080 Ti', stock: false, url: 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-ti-12gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6462956.p?skuId=6462956'},
+  {name: 'NVIDIA GeForce RTX 3090', stock: false, url: 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434'}
+]
 
-try{
+while(true){
 
-  console.log('sending request to server & awaiting response...')
+  // wait
+  await new Promise(resolve => setTimeout(resolve, millisecond_timer))
 
-  // NVIDIA GeForce RTX 3080 Ti
-  const response = await fetch('https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-ti-12gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6462956.p?skuId=6462956')
+  items.forEach(item => {
+
+    console.log('sending request to server...')
   
-  // in stock example
-  // const response = await fetch('https://www.bestbuy.com/site/pny-geforce-gt-730-2gb-ddr3-single-fan-graphics-card/6471621.p?skuId=6471621')
+    fetch(item.url)
+    .then(response => {
 
-  console.log(`server response: ${response.status}/${response.statusText}`)
-
-  if(response.status === 200 && response.statusText === 'OK'){
-
-    const body = await response.text()
-    //console.log(body)
-
-    body.includes('sold out') ? console.log('sold out') : console.log('in stock')
-
-  }
-}
-catch (error){
-
-  console.log(error)
-
+      console.log(`server response: ${response.status}/${response.statusText}`)
+      
+      if(response.status === 200){
+    
+        // extract body from response object
+        response.text()
+        .then(body => {
+          var stock_status = body.includes('sold out') ? false : true
+      
+          // if item's new stock status is changed
+          if(stock_status !== item.stock){
+            
+            // switch stock status in array
+            item.stock = !item.stock
+      
+            // send webhook to IFTTT
+            console.log('sending request to IFTTT...')
+            fetch(`https://maker.ifttt.com/trigger/stock_notifier_app/with/key/${process.env.IFTTT_KEY}?value1=${item.name}&value2=${stock_status ? 'in stock' : 'out of stock'}`)
+            .then(response => console.log(`IFTTT response: ${response.status}/${response.statusText}`))
+            .catch(error => console.log(`an error occured while sending request to IFTTT: ${error}`))
+          }
+        })
+        .catch(error => console.log(`error extracting body from server response: ${error}`))
+      }
+    })
+    .catch(error => console.log(`an error occured while sending request to server: ${error}`))
+  })
 }
